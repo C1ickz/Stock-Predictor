616a3fecf222478f9d4f938a97a9f76eaf355d9b
harrisrl2023 <46679450+harrisrl2023@users.noreply.github.com>
2019-10-10 23:38:44 -0400

    Grouped code into functions

--- SimpleStocksAndGraph.py
+++ SimpleStocksAndGraph.py
@@ -1,26 +1,32 @@
-import pandas_datareader.data as web
+import pandas_datareader as web
 import matplotlib.pyplot as plt
 import datetime as dt
 
-start = dt.datetime(2018, 1, 3)  # (YEAR, MONTH, DAY)
-end = dt.datetime(2019, 10, 9)
 
-ticker = input("input ticker (NOTE: This is not being error checked): ").upper()
-# f = web.DataReader(ticker, 'stooq', start, end) # if ticker does not work add ^ in front aka ^tkr
-f = web.DataReader(ticker, 'yahoo', start, end)  # if ticker does not work add ^ in front aka ^tkr
+def gatherData():
+    start = dt.datetime(2018, 1, 3)  # (YEAR, MONTH, DAY)
+    end = dt.datetime(2019, 10, 9)
+    ticker = input("input ticker (NOTE: This is not being error checked): ").upper()
+    df = web.DataReader(ticker, 'yahoo', start, end)  # if ticker does not work add ^ in front aka ^tkr
+    # print(df.tail(10))  # prints first 10 lines of stock data
+    # print("-----------")
+    # print(df['Adj Close'])
 
-print(f.tail(10))  # prints first 10 lines of stock data
-print("-----------")
-print(f['Adj Close'])
+    return df, ticker
 
-# try to graph
-wantToSee = input("Do you want to see the graph?(y or n): ")
-if wantToSee == 'y':
-	# f["Close"].plot()
-	plt.style.use('dark_background') # For more info on styles visit https://pythonprogramming.net/styles-matplotlib-tutorial/
-	f["Adj Close"].plot()
-	plt.title("Stock Prices of {}".format(ticker))
-	plt.ylabel("Price ($)")
-	plt.show()
-else:
-    print("exiting program...")
+
+def displayGraph(df):
+    # try to graph
+    wantToSee = input("Do you want to see the graph?(y or n): ")
+    if wantToSee == 'y':
+        plt.style.use('dark_background')
+        df[0]["Adj Close"].plot()
+        plt.title(f"Stock Prices of {df[1]}")
+        plt.ylabel("Price ($)")
+        plt.show()
+    else:
+        print("exiting program...")
+
+
+data = gatherData()
+displayGraph(data)
