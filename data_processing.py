bb782ab9a145eac182d88b00edb3a56e5c987cbe
Ryan <harrisrl2023@mountunion.edu>
2019-11-13 11:38:46 -0500

    Started code for graphing data

--- data_processing.py
+++ data_processing.py
@@ -11,9 +11,8 @@ from tensorflow.keras.callbacks import EarlyStopping
 
 # This python file will process the data and train it.
 # No prediction will be done in here
-
 df = pd.read_csv('tesla.csv')  # TODO make so user can choose csv
-df = df.set_index(df['Date'])
+dataset = df['Adj Close'].values
 most_recent = pd.Timestamp(df['Date'].max())
 trainingRange = str(most_recent - dt.timedelta(days=20))
 train = df.loc[:trainingRange, ['Adj Close']]
@@ -28,18 +27,19 @@ def to_sequences(seq_size, obs):
     y = []
 
     for i in range(len(obs) - seq_size - 1):
-        window = obs[i:(i + seq_size)]
+        window = obs[i:(i + seq_size), 0]
         x.append(window)
-        y.append(obs[i+seq_size])
+        y.append(obs[i + seq_size, 0])
     return np.array(x), np.array(y)
 
 
 X_train, Y_train = to_sequences(5, train_scaled)
 X_test, Y_test = to_sequences(5, test_scaled)
-print(X_test)
-X_train = np.reshape(X_train,(X_train.shape[0], X_train.shape[1], 1))
+X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
 X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
-
+print("=============================================================")
+print(f"tests shape is {test.shape}; X_trains shape is {X_train.shape} ; X_Tests shape is {X_test.shape}")
+print("================================================================================")
 print(X_train.shape, Y_train.shape)
 model = Sequential()
 model.add(LSTM(12, input_shape=(X_train.shape[1], 1), activation='relu'))
@@ -49,12 +49,26 @@ model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
 monitor = EarlyStopping(monitor='loss', min_delta=1e-3, patience=10,
                         verbose=1, mode='auto', restore_best_weights=True)
 
-
 model.fit(X_train, Y_train, validation_data=(X_test, Y_test),
           callbacks=[monitor], verbose=1, epochs=200)
-
+print("Shape of X_test", X_test.shape)
 # TODO Move code for predictions somewhere else
-prediction = model.predict(X_test)
-plt.plot(Y_test)
-plt.plot(prediction)
+train_predict = model.predict(X_train)
+test_predict = model.predict(X_test)
+
+train_predict_plot = np.empty_like(dataset)
+train_predict_plot[:, :] = np.nan
+train_predict_plot[5:len(train_predict) + 5, :] = train_predict
+
+test_predict_plot = np.empty_like(dataset)
+test_predict_plot[:, :] = np.nan
+test_predict_plot[len(train_predict) + (5*2)+1: len(dataset) -1, :] = test_predict
+plt.title("Stocks for TSLA")
+
+plt.plot(test)
+print(test)
+
+plt.tight_layout()
+plt.gcf().autofmt_xdate()
+
 plt.show()
