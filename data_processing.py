4b782c7f7077f5d30f561384dcd481d493b1960d
Ryan <harrisrl2023@mountunion.edu>
2019-11-24 01:32:08 -0500

    Fixed not plotting of last piece of data

--- data_processing.py
+++ data_processing.py
@@ -13,47 +13,54 @@ from tensorflow.keras.callbacks import EarlyStopping
 # No prediction will be done in here
 df = pd.read_csv('tesla.csv')  # TODO make so user can choose csv
 df = df.set_index(df['Date'])
-dataset = df['Adj Close']
+dataset = df['Adj Close'].values
 most_recent = pd.Timestamp(df['Date'].max())
-trainingRange = str(most_recent - dt.timedelta(days=20))
+trainingRange = str(most_recent - dt.timedelta(days=40))
 train = df.loc[:trainingRange, ['Adj Close']]
 test = df.loc[trainingRange:, ['Adj Close']]
-scaler = MinMaxScaler(feature_range=(0, 1))  # set values between 0 and 1
+scaler = MinMaxScaler(feature_range=(-1, 1))  # set values between 0 and 1
 train_scaled = scaler.fit_transform(train)
 test_scaled = scaler.transform(test)
 
-def to_sequences(seq_size, obs):
+
+def to_sequences(obs, window_size):
     x = []
     y = []
 
-    for i in range(len(obs) - seq_size - 1):
-        window = obs[i:(i + seq_size), 0]
+    for i in range(len(obs) - window_size - 1):
+        window = obs[i:(i + window_size)]
+        window = [[x] for x in window]
         x.append(window)
-        y.append(obs[i + seq_size, 0])
+        y.append(obs[i + window_size])
     return np.array(x), np.array(y)
 
 
-X_train, Y_train = to_sequences(5, train_scaled)
-X_test, Y_test = to_sequences(5, test_scaled)
+X_train, Y_train = to_sequences(train_scaled, 5)
+X_test, Y_test = to_sequences(test_scaled, 5)
 X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
 X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+print(X_test)
 print("=============================================================")
 print(f"tests shape is {test.shape}; X_trains shape is {X_train.shape} ; X_Tests shape is {X_test.shape}")
 print("================================================================================")
 print(X_train.shape, Y_train.shape)
 model = Sequential()
-model.add(LSTM(12, input_shape=(X_train.shape[1], 1), activation='relu'))
+model.add(LSTM(16, input_shape=(X_train.shape[1], 1), activation='relu'))
 model.add(Dropout(.2))
 model.add(Dense(1, activation='linear'))
-model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='mean_absolute_error', optimizer='adam', metrics=['accuracy'])
 monitor = EarlyStopping(monitor='loss', min_delta=1e-3, patience=10,
                         verbose=1, mode='auto', restore_best_weights=True)
 
 model.fit(X_train, Y_train, validation_data=(X_test, Y_test),
           callbacks=[monitor], verbose=1, epochs=200)
 print("Shape of X_test", X_test.shape)
+
+
 # TODO Move code for predictions somewhere else
-dataset = dataset.values.reshape(len(dataset), 1)
+
+
+dataset = dataset.reshape(len(dataset), 1)
 train_predict = model.predict(X_train)
 test_predict = model.predict(X_test)
 train_predict = scaler.inverse_transform(train_predict)
@@ -65,15 +72,14 @@ train_predict_plot[5:len(train_predict) + 5, :] = train_predict
 
 test_predict_plot = np.empty_like(dataset)
 test_predict_plot[:, :] = np.nan
-test_predict_plot[len(train_predict) + (5*2)+1: len(dataset) -1, :] = test_predict
+test_predict_plot[len(train_predict) + (5 * 2) + 2: len(dataset) , :] = test_predict
+print(test_predict_plot)
 plt.title("Stocks for TSLA")
 
 print(f"Test predict plots shape is {test_predict_plot.shape}")
-plt.plot(df['Date'],train_predict_plot)
-plt.plot(test)
-plt.plot(df['Date'],test_predict_plot)
+plt.plot(df['Date'], train_predict_plot)
+plt.plot(df['Date'], test_predict_plot)
 plt.tight_layout()
 plt.gcf().autofmt_xdate()
-
 
 plt.show()
