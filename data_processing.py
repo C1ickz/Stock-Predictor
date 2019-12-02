558b3f11829efa1611e3311b8b0b98ea124726de
Ryan <harrisrl2023@mountunion.edu>
2019-12-02 16:59:44 -0500

    Grouped data into functions

--- data_processing.py
+++ data_processing.py
@@ -11,21 +11,35 @@ from tensorflow.keras.callbacks import EarlyStopping
 
 # This python file will process the data and train it.
 # No prediction will be done in here
-df = pd.read_csv('tesla.csv')  # TODO make so user can choose csv
-df['Date'] = pd.to_datetime(df['Date'])
-dataset = list(zip(df['Date'], df['Adj Close'].values))
-most_recent = pd.Timestamp(df['Date'].max())
-print(most_recent)
-trainingRange = str(most_recent -  dt.timedelta(days=30))
-print(f"Train is using the data between {df['Date'].min()} and {(most_recent - dt.timedelta(days=20))}")
-print(f"Test is using the data between {most_recent - dt.timedelta(days=20)} and {most_recent}")
-print(f"The training range{(most_recent - (most_recent- dt.timedelta(days=20))).days} days")
-train = df.loc[:trainingRange, ['Adj Close']]
-test = df.loc[trainingRange:, ['Adj Close']]
-print(len(train))
-scaler = MinMaxScaler(feature_range=(0, 1))  # set values between 0 and 1
-train_scaled = scaler.fit_transform(train)
-test_scaled = scaler.transform(test)
+
+WINDOW_SIZE = 10
+
+
+def data_loader(filename):
+    df = pd.read_csv(filename)  # TODO make so user can choose csv
+    df['Date'] = pd.to_datetime(df['Date'])
+    dataset = list(zip(df['Date'], df['Adj Close'].values))
+
+    return df, dataset
+
+
+def train_test_split(dataset, df):
+    trainingRange = int(len(dataset) - 20)
+    print("The training range is", trainingRange)
+    train = df['Adj Close'].iloc[:trainingRange].values
+    test = df['Adj Close'].iloc[trainingRange:].values
+    train = np.reshape(train, (-1, 1))
+    test = np.reshape(test, (-1, 1))
+
+    return train, test
+
+
+def data_scaler(train, test):
+    scaler = MinMaxScaler(feature_range=(0, 1))  # set values between 0 and 1
+    train_scaled = scaler.fit_transform(train)
+    test_scaled = scaler.transform(test)
+
+    return train_scaled, test_scaled
 
 
 def to_sequences(data, window_size):
@@ -39,52 +53,61 @@ def to_sequences(data, window_size):
     return np.array(x), np.array(y)
 
 
-WINDOW_SIZE = 10
-X_train, Y_train = to_sequences(train_scaled, WINDOW_SIZE)
-X_test, Y_test = to_sequences(test_scaled, WINDOW_SIZE)
-X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
-X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
-print(X_test)
-print("=============================================================")
-print(f"tests shape is {test.shape}; X_trains shape is {X_train.shape} ; X_Tests shape is {X_test.shape}")
-print("================================================================================")
-print(X_train.shape, Y_train.shape)
-model = Sequential()
-model.add(LSTM(64, input_shape=(X_train.shape[1], 1), return_sequences=True, activation='relu'))
-model.add(Dropout(.2))
-model.add(LSTM(32))
-model.add(Dropout(.2))
-model.add(Dense(1, activation='linear'))
-model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])  # adagrad?
-monitor = EarlyStopping(monitor='loss', patience=10, verbose=1, restore_best_weights=True)
-model.fit(X_train, Y_train, callbacks=[monitor], epochs=200)
+def prepare_data(train_scaled, test_scaled):
+    X_train, Y_train = to_sequences(train_scaled, WINDOW_SIZE)
+    X_test, Y_test = to_sequences(test_scaled, WINDOW_SIZE)
+    X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+    X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+    print(X_test)
+    print("=============================================================")
+    print(f"X_trains shape is {X_train.shape} ; Y_Trains shape is {Y_train.shape}")
+    print(f"X_tests shape is {X_test.shape} ; Y_Tests shape is {Y_test.shape}")
+    print("================================================================================")
+    print(X_train.shape, Y_train.shape)
+
+
+def build_model(X_train, Y_train):
+    model = Sequential()
+    model.add(LSTM(64, input_shape=(X_train.shape[1], 1), return_sequences=True, activation='relu'))
+    model.add(Dropout(.2))
+    model.add(LSTM(32))
+    model.add(Dropout(.2))
+    model.add(Dense(1, activation='linear'))
+    model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])  # adagrad?
+    monitor = EarlyStopping(monitor='loss', patience=10, verbose=1, restore_best_weights=True)
+    model.fit(X_train, Y_train, callbacks=[monitor], epochs=200)
+
+    return model
+
+
+def save_model(model):
+    # TODO implement model saving
+    return model
+
 
 # TODO Move code for predictions somewhere else
-original = pd.DataFrame(dataset, columns=['Date', 'Adj Close'])
-original = original.set_index('Date')
-print(original)
-print(f"{len(train)} {len(test)}")
-print(f"{X_train.shape} {X_test.shape}")
-train_predict = model.predict(X_train)
-test_predict = model.predict(X_test)
-train_predict = scaler.inverse_transform(train_predict)
-test_predict = scaler.inverse_transform(test_predict)
-train_predict_plot = np.empty_like(dataset)
-train_predict_plot[:, :] = np.nan
-train_predict_plot[WINDOW_SIZE:len(train_predict) + WINDOW_SIZE, :] = train_predict
 
-test_predict_plot = np.empty_like(dataset)
-test_predict_plot[:, :] = np.nan
-test_predict_plot[len(train_predict) + (WINDOW_SIZE * 2) + 1: len(dataset) - 1, :] = test_predict
-plt.title("Stocks for TSLA")
+def graph_format(train_predict, dataset):
+    train_predict_plot = np.empty_like(dataset)
+    train_predict_plot[:, :] = np.nan
+    train_predict_plot[WINDOW_SIZE:len(train_predict) + WINDOW_SIZE, :] = train_predict
 
-print(f"Test predict plots shape is {test_predict_plot.shape}")
-plt.plot(df['Date'], train_predict_plot)
-plt.plot(df['Date'], test_predict_plot)
-plt.plot(df['Date'], df['Adj Close'])
-plt.xlabel('Date')
-plt.ylabel('Adj Close Price')
-plt.tight_layout()
-plt.gcf().autofmt_xdate()
+    test_predict_plot = np.empty_like(dataset)
+    test_predict_plot[:, :] = np.nan
+    test_predict_plot[len(train_predict) + (WINDOW_SIZE * 2) + 1: len(dataset) - 1, :] = test_predict
 
-plt.show()
+
+def graph_data(train_predict_plot, test_predict_plot):
+    plt.title("Stocks for TSLA")
+
+    print(f"Test predict plots shape is {test_predict_plot.shape}")
+    plt.plot(df['Date'], train_predict_plot, "-b", label="Train predict")
+    plt.plot(df['Date'], test_predict_plot, "-y", label="Test predict")
+    plt.plot(df['Date'], df['Adj Close'], "-g", label="Original")
+    plt.xlabel('Date')
+    plt.ylabel('Adj Close Price')
+    plt.legend(loc="upper left")
+    plt.tight_layout()
+    plt.gcf().autofmt_xdate()
+
+    plt.show()
---
+++ prediction.py
@@ -0,0 +1,10 @@
+from data_processing import data_loader
+
+df = data_loader('tesla.csv')[0]
+
+
+
+#train_predict = model.predict(X_train)
+#test_predict = model.predict(X_test)
+#train_predict = scaler.inverse_transform(train_predict)
+#test_predict = scaler.inverse_transform(test_predict)
